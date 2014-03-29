79c8deebb3f0571f2439dc86ac62ab4aa2621e13
Tim VanFosson <tvanfosson@gmail.com>
2014-03-29 11:26:08 -0500

    Initial project set up

---
+++ AjaxExample/Scripts/config.js
@@ -0,0 +1,13 @@
+﻿requirejs.config({
+    baseUrl: "scripts",
+    paths: {
+        'jquery': '//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min',
+        'jquery-ui': '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min',
+        'bootstrap': '//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.'
+    },
+    shim: {
+        /* Set bootstrap dependencies (just jQuery) */
+        'bootstrap': ['jquery'],
+        'jquery-ui': ['jquery']
+    }
+});
